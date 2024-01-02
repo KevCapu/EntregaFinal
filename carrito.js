ced0ee5a08b34b8fdc6be09f267b9acb0c74ea01
Kevin Caputo <kev.caputo63@gmail.com>
2024-01-02 20:48:54 -0300

    EntregaFinal2

--- carrito.js
+++ carrito.js
@@ -5,18 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
         { nombre: 'Digimon World 2', precio: 39},
         { nombre: 'Strider 2', precio: 20},
         { nombre: 'Persona 3', precio: 29},
-        { nombre: 'DarkWatch', precio: 19}  ,      
+        { nombre: 'DarkWatch', precio: 19},
         { nombre: 'Black', precio: 29 },
         { nombre: 'Mortal Kombat Armaggedon', precio: 39},
         { nombre: 'Borderlands', precio: 49},
-        { nombre: 'Darksiders', precio: 19}  ,      
+        { nombre: 'Darksiders', precio: 19},
         { nombre: 'Shadow of Mordor', precio: 29 },
         { nombre: 'Fallout 3', precio: 39},
         { nombre: 'Ghostbusters', precio: 19},
-        { nombre: 'RememberMe', precio: 29}  ,      
+        { nombre: 'RememberMe', precio: 29},
         { nombre: 'Left 4 Dead', precio: 9 },
         { nombre: 'Operation Darkness', precio: 19},
-
     ];
 
     const carrito = [];
@@ -62,13 +61,76 @@ document.addEventListener('DOMContentLoaded', function () {
         btn.addEventListener('click', agregarAlCarrito);
     });
 
-    descargarCarritoBtn.addEventListener('click', descargarCarrito);
+    descargarCarritoBtn.addEventListener('click', function () {
+        descargarCarrito();
+    });
 
     function descargarCarrito() {
-        alert('Descarga iniciada, Muchas gracias por confiar en El Gremio33!');
+        Swal.fire({
+            imageUrl: "./img/Botonalert.png",
+            background:  "#000000",
+            imageAlt: "gracias por confiar en El Gremio 33",
+            confirmButtonColor: '#EFCA08',
+        });
+
         carrito.length = 0;
         actualizarCarrito();
     }
-
+    
     actualizarTotal();
+
+const botonLogin = document.getElementById('login');
+
+const usuario = {
+    nombreUsuario:'',
+    contraseña: ''
+
+};
+
+botonLogin.addEventListener('click', () => {
+    swal.fire({
+        title: 'login de Usuario',
+        text: 'Bienvenio al Gremio33',
+        inputLabel: 'Ingrese su email',
+        inputPlaceholder: 'ejemplo@gmail.com',
+        input: 'email',
+        confirmButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Enviar',
+        cancelButtonText: 'Cancelar',
+        background:  "#000000",
+        cancelButtonColor: '#FF0000',
+        confirmButtonColor: '#EFCA08',
+        color:'#EFCA08',
+
+    }).then((result) => {
+        if (result.isConfirmed) {
+            usuario.nombreUsuario = result.value;
+            swal.fire({
+                title: 'Login de Usuario',
+                text: "contraseña",
+                inputLabel: "ingrese su contraseña",
+                input: 'password',
+                inputPlaceholder: 'X;x;1;?',
+                confirmButtonText: 'Enviar',
+                cancelButtonText: 'Cancelar',
+                showCancelButton: true,
+                background: "#000000",
+                cancelButtonColor: '#FF0000',
+                confirmButtonColor: '#EFCA08',
+                color: '#EFCA08'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    swal.fire({
+                        title: 'Bienvenido al Gremio33',
+                        icon: 'success',
+                        background: "#000000",
+                        color: '#EFCA08',
+                        confirmButtonColor: '#EFCA08',
+                    });
+                }
+            });
+        }
+    });
+});
 });
